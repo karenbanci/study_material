42185b87c4719617db52c407950b7ff3e6edf7f1
karen banci <karenbanci1@gmail.com>
2022-03-09 16:10:59 -0800

    add promises

---
+++ front_end.js
@@ -0,0 +1,51 @@
+//https://www.youtube.com/watch?v=kDd33VxaxaY
+
+// 1) What does this function print?
+// let number;
+// for (let i = 0; i < 5; i++){
+//   // estamos atribuindo i ao numero que é uma variavel global
+//   number = i;
+//   //(tem tempo definido de 1s para cada for)
+//   setTimeout(function() {
+//     console.log(number);
+//   }, 1000);
+// }//4(5)
+
+// for (let i = 0; i < 5; i++) {
+//   //essa funcao é para cronometrar o tempo de execusao (tem tempo definido)
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// for (var i = 0; i < 5; i++) {
+//   //essa funcao é para cronometrar o tempo de execusao (tem tempo definido)
+//   setTimeout(function () {
+//     console.log(number);
+//   }, 1000);
+// }
+// 4(5)
+
+/*
+if eu tenho uma array que tenha valores menores que 5
+então ele vai imprimir cada número acaixo de 5, ou seja :
+
+*/
+
+
+
+
+// 2) Create a user defined table, accepting rows and columns:
+/*
+number of rows = 4
+number of columns = 5
+
+'submit''
+
+print this:
+
+1   8   9   16  17
+2   7   10  15  18
+3   6   11  14  19
+4   5   12  13  20
+*/
