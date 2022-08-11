6e520fe430961cf28a0b7225f7f25e8f7811f470
Karen <karenbanci1@gmail.com>
2022-08-10 17:44:50 -0700

    add coments

--- exercices-leetcode/169-majority-element.js
+++ exercices-leetcode/169-majority-element.js
@@ -14,30 +14,36 @@ Output: 2
 
 var majorityElement = function (nums) {
   let n = nums.length;
+  // metade is a half of array
+  //half means that if the number repeats more than half of times, example: If array size is 5, the number has to repeat more than 3 times (2.5 rounds to 3)
   let metade = n / 2;
 
+  // new object, is empty
   const objeto = new Object();
 
   for (let i = 0; i < n; i++) {
-  // criar elementos em um objeto
-    if(objeto[nums[i]]){
+    // encrease value into the object, how many numbers repeated
+    if (objeto[nums[i]]) {
+      // if exist this key, encrease +1
       objeto[nums[i]]++;
+      // else, keep the value 1
     } else {
       objeto[nums[i]] = 1;
     }
-
   }
   console.log(objeto);
+  // here I will do check
   let keys = Object.keys(objeto);
 
+  // check each key
   for (let j = 0; j < keys.length; j++) {
-    if(objeto[keys[j]] >= metade){
-      return keys[j]
+    // if the number of numbers is greater than half of the array
+    if (objeto[keys[j]] >= metade) {
+      // return key
+      return keys[j];
     }
     console.log(objeto);
   }
-
-
 };
 
 const nums = [8, 8, 7, 7, 7];
