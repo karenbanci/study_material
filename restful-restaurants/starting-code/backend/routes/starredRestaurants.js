55e1b1ea2c9362c71270a91cea891d8020b11199
Karen <karenbanci1@gmail.com>
2023-01-05 11:52:01 -0800

    add instructions

--- restful-restaurants/starting-code/backend/routes/starredRestaurants.js
+++ restful-restaurants/starting-code/backend/routes/starredRestaurants.js
@@ -48,24 +48,92 @@ router.get("/", (req, res) => {
 /**
  * Feature 7: Getting a specific starred restaurant.
  */
+router.get("/:id", (req, res) => {
+  // Join the starred data with the all restaurants data to get the name of the starred restaurant.
+  // Find the restaurant in the list of restaurants.
+  const { id } = req.params;
 
+  // If the restaurant doesn’t exist, send a status code to the client to let it know the restaurant was not found.
+  if(!restaurant){
+    res.status(404).send("Restaurant not found");
+    return;
+  }
+
+  // Otherwise, create an object with the starred restaurant’s id, comment, and name, and send the restaurant data to the front-end.
+  res.json(restaurant);
+});
 
 
 /**
  * Feature 8: Adding to your list of starred restaurants.
  */
+router.post("/", (req, res) => {
+  //   Find the restaurant in the list of starred restaurants.
+  const {body} = req;
+  const {id} = body;
 
+  const restaurant = ALL_RESTAURANTS.find((restaurant) => restaurant.id === id);
+
+  // If the restaurant doesn’t exist, send a status code to the client to let it know the restaurant was not found.
+  if(!restaurant){
+    res.status(404).send("Restaurant not found");
+    return;
+  }
+  // Otherwise, proceed with adding a restaurant to your starred restaurants list:
+  // Generate a unique id for the new starred restaurant.
+  const newId = uuidv4();
+
+  // Create a record for the new starred restaurant.
+  const newStarredRestaurant = {
+    id: newId,
+    restaurantId: id,
+    comment: null
+  };
+  // Push the new record into STARRED_RESTAURANTS.
+  STARRED_RESTAURANTS.push(newStarredRestaurant);
+  // Set a success status code and send the restaurant data to the front-end.
+  res.status(200).send({
+    id: newStarredRestaurant.id,
+    comment: newStarredRestaurant.comment,
+    name: restaurant.name
+  });
+});
 
 
 /**
  * Feature 9: Deleting from your list of starred restaurants.
  */
 
+router.delete("/:id", (req, res) => {
+  //   Use the .filter() method to remove this restaurant from your list of starred restaurants and save this list in a variable.
+  const {id} = req.params;
+  const newListOfStarredRestaurant = STARRED_RESTAURANTS.filter((restaurant) => restaurant.id !== id);
+  // If the restaurant doesn’t exist, send a status code to the client to let it know the restaurant was not found.
+  if(STARRED_RESTAURANTS.length === newListOfStarredRestaurant.length){
+    res.status(404).send("Restaurant not found");
+    return;
+  }
+  // Otherwise, reassign STARRED_RESTAURANTS with the updated list of starred restaurants that you stored in a variable.
+  STARRED_RESTAURANTS = newListOfStarredRestaurant;
+  res.sendStatus(200);
+});
 
 /**
  * Feature 10: Updating your comment of a starred restaurant.
  */
-
-
+router.put("/:id", (req, res) => {
+  //   Find the restaurant in the list of starred restaurants.
+  const {id} = req.params;
+  const newComment = req.body;
+  // If the restaurant doesn’t exist, send a status code to the client to let it know the restaurant was not found.
+  if(!restaurant){
+    res.status(404).send("Restaurant not found");
+    return;
+  }
+  // Otherwise, update the restaurant’s comment with the comment included in the request body.
+  restaurant.comment = newComment;
+  // Send a success status code to the client.
+  res.sendStatus(200);
+});
 
 module.exports = router;
