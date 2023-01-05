a440366b9422801c42dcbcbdc890f44c3a8d0c14
Karen <karenbanci1@gmail.com>
2023-01-04 23:18:29 -0800

    add addNewBook async

--- creating-rest-api/starting-code/frontend/src/api/books.js
+++ creating-rest-api/starting-code/frontend/src/api/books.js
@@ -7,3 +7,20 @@
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
 
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
+import { API_ENDPOINT } from "./index.js";
+
+export const addNewBook = async (newTitle, newStart, newEnd) {
+  const response = await fetch(`${API_ENDPOINT}/books`, {
+    method: "POST",
+    body: JSON.stringify({
+      title: newTitle,
+      start: newStart,
+      end: newEnd,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+  const newBook = await response.json();
+  return newBook;
+}
