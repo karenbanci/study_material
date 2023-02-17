dd4e5daa331e93a2ad5bddb270b8edc3d96da0d9
Karen <karenbanci1@gmail.com>
2023-02-16 20:20:41 -0800

    add transform object

--- mini-projects/three-js/05-transform-objects/src/script.js
+++ mini-projects/three-js/05-transform-objects/src/script.js
@@ -9,12 +9,39 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
 
-/**
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+cube2.position.x = -2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+cube3.position.x = 2;
+group.add(cube3);
+
+//Axes helper - é um eixo de coordenadas
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+
+/*
  * Sizes
  */
 const sizes = {
@@ -27,7 +54,12 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-scene.add(camera)
+// camera.position.y = 1;
+// camera.position.x = 1;
+scene.add(camera);
+
+// camera.lookAt(mesh.position);
+
 
 /**
  * Renderer
@@ -37,3 +69,31 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
+
+
+
+/*
+para criar um cubo simples
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh);
+
+// Position
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6, 1);
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.5;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.5, 0.5);
+
+// Rotation
+mesh.rotation.reorder('YXZ'); // reordena a ordem de rotação importante deixar antes de setar a rotação
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+*/
