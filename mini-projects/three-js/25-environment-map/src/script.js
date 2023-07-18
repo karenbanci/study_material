89f122625f68c80a76da7a6b9a8106b7378f8e95
Karen <karenbanci1@gmail.com>
2023-07-17 19:04:08 -0700

    finish lesson 25

--- mini-projects/three-js/25-environment-map/src/script.js
+++ mini-projects/three-js/25-environment-map/src/script.js
@@ -1,91 +1,195 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'lil-gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "lil-gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const rgbeLoader = new RGBELoader();
+const exrLoader = new EXRLoader();
+const textureLoader = new THREE.TextureLoader();
 
 /**
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
+const global = {}
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
+
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () => {
+  scene.traverse((child) => { // cada filho da cena
+
+    // aqui eu verifico se o filho é uma mesh e se o material é uma instância de MeshStandardMaterial
+    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+
+      // console.log('filho', child );
+      // aqui eu atualizo o material do filho com o environmentMap e a intensidade
+      child.material.envMap = environmentMap;
+      child.material.envMapIntensity = global.envMapIntensity;
+      child.material.needsUpdate = true; // atualiza o material
+
+    }
+
+  })
+};
+
+/**
+ * Environment map
+ */
+scene.backgroundBlurriness = 0; // essa linha deixa o background com blur (fosco)
+scene.environmentIntensity = 1;
+
+gui.add(scene, 'backgroundBlurriness').min(0).max(1).step(0.001).onChange(updateAllMaterials);
+gui.add(scene, 'environmentIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials);
+
+// Global intesity
+global.envMapIntensity = 1;
+gui.add(global, 'envMapIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials);
+
+// LDR cube texture
+// const environmentMap = cubeTextureLoader.load([
+//   // 6 images
+//   "/environmentMaps/0/px.png",
+//   "/environmentMaps/0/nx.png",
+//   "/environmentMaps/0/py.png",
+//   "/environmentMaps/0/ny.png",
+//   "/environmentMaps/0/pz.png",
+//   "/environmentMaps/0/nz.png",
+// ]);
+
+// scene.environment = environmentMap; // essa linha adiciona o environmentMap na cena inteira
+// scene.background = environmentMap;
+
+// HDR (RGBE)
+// rgbeLoader.load("/environmentMaps/blender-2k.hdr", (environmentMap) => {
+//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+
+//   scene.background = environmentMap;
+//   scene.environment = environmentMap;
+// });
+
+// // EXR (OpenEXR)
+// exrLoader.load("/environmentMaps/nvidiaCanvas-4k.exr", (environmentMap) => {
+//   environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+
+//   scene.background = environmentMap;
+//   scene.environment = environmentMap;
+// });
+
+// LDR equirectangular
+// const environmentMap = textureLoader.load('/environmentMaps/holographic_disneyworld_with_just_one_castle_tree.jpg');
+// environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+// environmentMap.colorSpace = THREE.SRGBColorSpace;
+// scene.background = environmentMap;
+// scene.environment = environmentMap;
+
 
 /**
  * Torus Knot
  */
 const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-    new THREE.MeshBasicMaterial()
-)
-torusKnot.position.y = 4
-scene.add(torusKnot)
+  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+  new THREE.MeshStandardMaterial({
+    roughness: 0.3,
+    metalness: 1,
+    color: 0xaaaaaa,
+  })
+);
+
+// torusKnot.material.envMap = environmentMap;
+torusKnot.position.y = 4;
+torusKnot.position.x = -4;
+scene.add(torusKnot);
+
+/**
+ * Models
+ */
+gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  gltf.scene.scale.set(10, 10, 10);
+  scene.add(gltf.scene);
+
+  updateAllMaterials() // essa linha atualiza todos os materiais do modelo
+});
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 5, 4)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4, 5, 4);
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.y = 3.5
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-const tick = () =>
-{
-    // Time
-    const elapsedTime = clock.getElapsedTime()
+const clock = new THREE.Clock();
+const tick = () => {
+  // Time
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
