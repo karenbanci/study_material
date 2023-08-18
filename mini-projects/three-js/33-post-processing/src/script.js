2f7abf27329e9031af56d4141daccf8cba3f507f
Karen <karenbanci1@gmail.com>
2023-08-17 21:18:58 -0700

    finish lesson 33

--- mini-projects/three-js/33-post-processing/src/script.js
+++ mini-projects/three-js/33-post-processing/src/script.js
@@ -1,7 +1,25 @@
 import * as THREE from 'three'
+import * as dat from 'lil-gui'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'lil-gui'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader'
+import { GammaCorrectionShader } from "three/examples/jsm/shaders/GammaCorrectionShader.js";
+import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass'
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass.js";
+
+
+// console.log(EffectComposer);
+
+/**
+ * Post-processing is about adding effects on the final image and we usually
+ * use it for film making, but we can ddo it in WebGL as well. It can be subtle to improve
+ * the image slightly or to create huge effects
+ */
 
 /**
  * Base
@@ -90,17 +108,21 @@ const sizes = {
 
 window.addEventListener('resize', () =>
 {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  // Update effect composer - a imagem fica pixada na tela grande
+  effectComposer.setSize(sizes.width, sizes.height);
+  effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 })
 
 /**
@@ -128,7 +150,153 @@ renderer.useLegacyLights = false
 renderer.toneMapping = THREE.ReinhardToneMapping
 renderer.toneMappingExposure = 1.5
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)) // sempre limitar para 2
+
+/**
+ * Post-processing (https://caniuse.com/webgl2)
+ */
+// Render Target - improve the aliasing
+// console.log(renderer.getPixelRatio());
+const renderTarget = new THREE.WebGLRenderTarget(800, 600, {
+  // reactivate the anti-aliasing (escolher o menor valor possível que funcione por causa do desempenho)
+  samples: renderer.getPixelRatio() === 1 ? 2 : 0
+});
+
+const effectComposer = new EffectComposer(renderer)
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+effectComposer.setSize(sizes.width, sizes.height)
+
+// Render pass
+const renderPass = new RenderPass(scene, camera)
+effectComposer.addPass(renderPass)
+
+// Dot screen pass
+const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false
+effectComposer.addPass(dotScreenPass)
+
+// Glitch pass - a tela fica piscando parecendo que está com defeito (adorei)
+const glitchPass = new GlitchPass()
+glitchPass.goWild = false // fica repetindo o efeito
+glitchPass.enabled = false
+effectComposer.addPass(glitchPass)
+
+// RGB Shift Pass - o efeito de rgb shift é bem legal
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+rgbShiftPass.enabled = false
+effectComposer.addPass(rgbShiftPass)
+
+// Gamma correction pass - o efeito de gamma correction é bem legal
+const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
+effectComposer.addPass(gammaCorrectionPass);
+
+// Unreal Bloom Pass - o efeito de unreal
+const unrealBloomPass = new UnrealBloomPass();
+unrealBloomPass.strength = 0.3;
+unrealBloomPass.radius = 1;
+unrealBloomPass.threshold = 0.6;
+effectComposer.addPass(unrealBloomPass);
+
+gui.add(unrealBloomPass, 'enabled').name('unrealBloomPass')
+gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001).name('unrealBloomPassStrength')
+gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001).name('unrealBloomPassRadius')
+gui.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.001).name('unrealBloomPassThreshold')
+
+//  Tint pass
+const TintShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    uTint: { value: null },
+  },
+  vertexShader: `
+        varying vec2 vUv;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+            vUv = uv;
+        }
+    `,
+  fragmentShader: `
+        uniform sampler2D tDiffuse;
+        uniform vec3 uTint;
+
+        varying vec2 vUv;
+
+        void main()
+        {
+            vec4 color = texture2D(tDiffuse, vUv);
+            color.rgb += uTint;
+
+            gl_FragColor = color;
+        }
+    `,
+};
+
+const tintPass = new ShaderPass(TintShader)
+tintPass.material.uniforms.uTint.value = new THREE.Vector3()
+effectComposer.addPass(tintPass)
+
+gui.add(tintPass.material.uniforms.uTint.value, 'x').min(-1).max(1).step(0.001).name('Red')
+gui.add(tintPass.material.uniforms.uTint.value, 'y').min(-1).max(1).step(0.001).name('Green')
+gui.add(tintPass.material.uniforms.uTint.value, 'z').min(-1).max(1).step(0.001).name('Blue')
+
+
+
+//  Displacement pass
+const DisplacementShader = {
+  uniforms: {
+    tDiffuse: { value: null },
+    uNormalMap: { value: null },
+    // uTime: { value: null },
+  },
+  vertexShader: `
+        varying vec2 vUv;
+
+        void main()
+        {
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+            vUv = uv;
+        }
+    `,
+  fragmentShader: `
+        uniform sampler2D tDiffuse;
+        uniform sampler2D uNormalMap;
+        // uniform float uTime;
+
+        varying vec2 vUv;
+
+        void main()
+        {
+            vec3 normalColor = texture2D(uNormalMap, vUv).xyz * 2.0 - 1.0;
+
+            vec2 newUv = vUv + normalColor.xy * 0.1;
+            vec4 color = texture2D(tDiffuse, newUv);
+
+            vec3 lightDirection = normalize(vec3(- 1.0, 1.0, 0.0));
+            float lightness = clamp(dot(normalColor, lightDirection), 0.0, 1.0);
+            color.rgb += lightness * 2.0;
+
+            gl_FragColor = color;
+        }
+    `,
+};
+
+const displacementPass = new ShaderPass(DisplacementShader);
+displacementPass.material.uniforms.uNormalMap.value = textureLoader.load('/textures/interfaceNormalMap.png'); // efeito de profundidade na imagem (tipo um vidro)
+// displacementPass.material.uniforms.uTime.value = 0;
+effectComposer.addPass(displacementPass);
+
+// SMAA pass - anti-alias - é aplicado quando samples: renderer.getPixelRatio() nao é usado por causa do suporte do navegador
+// console.log(renderer.capabilities);
+if(renderer.getPixelRatio() === 1 && renderer.capabilities.isWebGL2){
+  const smaaPass = new SMAAPass()
+  effectComposer.addPass(smaaPass)
+  console.log('using smaa');
+
+}
 
 /**
  * Animate
@@ -139,14 +307,21 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Update passes
+    // displacementPass.material.uniforms.uTime.value = elapsedTime
+
     // Update controls
     controls.update()
 
     // Render
-    renderer.render(scene, camera)
+    // renderer.render(scene, camera)
+    effectComposer.render()
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
+
+
+
 }
 
 tick()
