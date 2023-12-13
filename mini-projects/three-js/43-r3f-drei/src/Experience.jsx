a88569b359a65668ca6b15c7ca421afd62c012f7
Karen <karenbanci1@gmail.com>
2023-12-13 11:21:49 -0800

    Update dependencies and styles

--- mini-projects/three-js/43-r3f-drei/src/Experience.jsx
+++ mini-projects/three-js/43-r3f-drei/src/Experience.jsx
@@ -1,33 +1,83 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+  MeshReflectorMaterial,
+  Text,
+  Float,
+  Html,
+  PivotControls,
+  TransformControls,
+  OrbitControls,
+} from "@react-three/drei";
+import { useRef } from "react";
 
-extend({ OrbitControls })
+export default function Experience() {
+  const cubeRef = useRef();
+  const sphereRef = useRef(); // create a reference to the sphere to use it in the PivotControls
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
+  return (
+    <>
+      <OrbitControls makeDefault />
 
-    return <>
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={1}
+        axisColors={["#9381ff", "#ff4d6d", "#7ae582"]}
+        scale={100}
+        fixed={true}
+      >
+        <mesh ref={sphereRef} position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="label"
+            center
+            distanceFactor={8}
+            occlude={[sphereRef, cubeRef]}
+          >
+            Karen 👩🏻‍💻
+          </Html>
         </mesh>
+      </PivotControls>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+      <mesh ref={cubeRef} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+      <TransformControls object={cubeRef} mode="translate" />
 
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <MeshReflectorMaterial
+          color="greenyellow"
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.75}
+        />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+      </mesh>
+      <Float speed={5} floatIntensity={2}>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          fontSize={1}
+          color="salmon"
+          position-y={2}
+          maxWidth={2}
+          textAlign="center"
+        >
+          Jesus loves us!
+        </Text>
+      </Float>
     </>
+  );
 }
+
+/* NOTES
+line 10 - makeDefault to prevent the camera moving when I am using TransformControls
+
+*/
