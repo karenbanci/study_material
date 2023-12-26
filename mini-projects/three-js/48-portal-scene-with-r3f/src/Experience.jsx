0eb1790157667b48fb9208aec5bfc36936a0654b
Karen <karenbanci1@gmail.com>
2023-12-26 13:37:08 -0800

    Update Experience component with portal scene and shaders

--- mini-projects/three-js/48-portal-scene-with-r3f/src/Experience.jsx
+++ mini-projects/three-js/48-portal-scene-with-r3f/src/Experience.jsx
@@ -1,15 +1,82 @@
-import { OrbitControls } from '@react-three/drei'
+import * as THREE from "three";
+import {
+  useGLTF,
+  OrbitControls,
+  useTexture,
+  Center,
+  Sparkles,
+  shaderMaterial,
+} from "@react-three/drei";
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
-export default function Experience()
-{
-    return <>
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#ffffff"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
 
-        <OrbitControls makeDefault />
+// send an object with the property and value being PortalMaterial
+extend({ PortalMaterial });
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
+export default function Experience() {
+  // access nodes of gltf file
+  const { nodes } = useGLTF("./model/portal.glb");
+
+  const bakedTexture = useTexture("./model/baked.jpg");
+  // this line will flip the texture vertically because it was wrong way around
+  // is the same as map-flipY={false} inside meshBasicMaterial
+  bakedTexture.flipY = false;
+
+  const portalMaterial = useRef();
+  useFrame((state, delta) => {
+    portalMaterial.current.uTime += delta;
+  });
+
+  return (
+    <>
+      <color args={["#030202"]} attach="background" />
+
+      <OrbitControls makeDefault />
+
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          <portalMaterial ref={portalMaterial} />
         </mesh>
 
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
+      </Center>
     </>
+  );
 }
