0bfa37e3dba07af408413aed9020719d4ef52566
Karen <karenbanci1@gmail.com>
2023-12-27 22:26:45 -0800

    Add physics functionality with @react-three/rapier

--- mini-projects/three-js/52-physics-with-r3f/src/Experience.jsx
+++ mini-projects/three-js/52-physics-with-r3f/src/Experience.jsx
@@ -1,31 +1,110 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  Physics,
+  RigidBody,
+  CuboidCollider,
+  BallCollider,
+} from "@react-three/rapier";
+import { useRef } from "react";
 
-export default function Experience()
-{
-    return <>
+// console.log("Physics", Physics, "RigidBody", RigidBody);
 
-        <Perf position="top-left" />
+export default function Experience() {
+  const cube = useRef();
 
-        <OrbitControls makeDefault />
+  const cubeJump = () => {
+    cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    cube.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+  return (
+    <>
+      <Perf position="top-left" />
 
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      <Physics debug gravity={[0, -9.08, 0]}>
+        {/* --------- Bola --------- */}
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
-        </mesh>
+          </mesh>
+        </RigidBody>
 
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
+        {/*  --------- Cubo  --------- */}
+        <RigidBody ref={cube} position={[1.5, 2, 0]} gravityScale={1}>
+          <mesh castShadow onClick={cubeJump}>
             <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+          </mesh>
+        </RigidBody>
 
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
+        {/*  --------- Piso  --------- */}
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
             <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
+  );
 }
+
+/**
+<RigidBody colliders="trimesh"> -- é para deixar o collider na forma do torus
+  <mesh
+    castShadow
+    position={[0, 1, -0.25]}
+    rotation={[Math.PI * 0.5, 0, 0]}
+  >
+    <torusGeometry args={[1, 0.5, 16, 32]} />
+    <meshStandardMaterial color="mediumpurple" />
+  </mesh>
+</RigidBody>
+
+  -- pesquisar mais sobre o colliders
+BallCollider https://rapier.rs/javascript3d/classes/Ball.html
+CuboidCollider https://rapier.rs/javascript3d/classes/Cuboid.html
+RoundCuboidCollider https://rapier.rs/javascript3d/classes/RoundCuboid.html
+CapsuleCollider https://rapier.rs/javascript3d/classes/Capsule.html
+ConeCollider https://rapier.rs/javascript3d/classes/Cone.html
+CylinderCollider https://rapier.rs/javascript3d/classes/Cylinder.html
+ConvexHullCollider https://rapier.rs/javascript3d/classes/ConvexPolyhedron.html
+TrimeshCollider https://rapier.rs/javascript3d/classes/TriMesh.html
+HeightfieldCollider https://rapier.rs/javascript3d/classes/Heightfield.html
+
+------- Torus Geometry -------
+<CuboidCollider args={[1.5, 1.5, 0.5]} />
+  <CuboidCollider
+    args={[0.25, 1, 0.25]}
+    position={[0, 0, 1]}
+    rotation={[-Math.PI * 0.35, 0, 0]}
+
+Donuts
+        <RigidBody
+          colliders={false}
+          position={[0, 1, -0.25]}
+          rotation={[Math.PI * 0.5, 0, 0]}
+        >
+          <BallCollider args={[1.5]} />
+          <mesh castShadow>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+/>
+
+------------- LINKS -------------
+https://rapier.rs/javascript3d/classes/RoundCuboid.html
+https://rapier.rs/javascript3d/classes/RigidBody.html
+*/
