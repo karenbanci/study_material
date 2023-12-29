42e8451c7ab6090e7fdaac636c7a0b7378b4f39e
Karen <karenbanci1@gmail.com>
2023-12-28 19:16:14 -0800

    Add @react-three/rapier physics library and implement player movement

---
+++ mini-projects/three-js/53-create-a-game-with-r3f/src/Player.jsx
@@ -0,0 +1,124 @@
+import { RigidBody, useRapier } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { useRef, useEffect, useState } from "react";
+import * as THREE from "three";
+// import * as RAPIER from "@dimforge/rapier3d-compat";
+// console.log(RAPIER);
+
+export default function Player() {
+  const body = useRef();
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const { rapier, world } = useRapier();
+  const rapierWorld = world;
+
+  const [smoothedCameraPosition] = useState(
+    () => new THREE.Vector3(10, 10, 10)
+  );
+  const [smoothedCameraTarget] = useState(() => new THREE.Vector3());
+
+  const jump = () => {
+    const origin = body.current.translation();
+    origin.y -= 0.31;
+
+    const direction = { x: 0, y: -1, z: 0 };
+    const ray = new rapier.Ray(origin, direction);
+    const hit = rapierWorld.castRay(ray, 10, true);
+    // console.log(hit.toi);
+
+    if (hit.toi < 0.15) {
+      body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribeJump = subscribeKeys(
+      // function to call when jump is pressed
+      (state) => state.jump,
+      (value) => {
+        if (value) {
+          jump();
+        }
+      }
+    );
+
+    return () => {
+      unsubscribeJump();
+    };
+  }, []);
+
+  useFrame((state, delta) => {
+    /**
+     * Constrols
+     */
+    const { forward, backward, leftward, rightward } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 0.6 * delta;
+    const torqueStrength = 0.2 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+    }
+    if (rightward) {
+      impulse.x += impulseStrength;
+      torque.z -= torqueStrength;
+    }
+
+    if (backward) {
+      impulse.z += impulseStrength;
+      torque.x += torqueStrength;
+    }
+    if (leftward) {
+      impulse.x -= impulseStrength;
+      torque.z += torqueStrength;
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
+
+    /**
+     * Camera
+     */
+    const bodyPosition = body.current.translation();
+
+    // destination
+    const cameraPosition = new THREE.Vector3();
+    cameraPosition.copy(bodyPosition);
+    cameraPosition.z += 2.25;
+    cameraPosition.y += 0.65;
+
+    const cameraTarget = new THREE.Vector3();
+    cameraTarget.copy(bodyPosition);
+    cameraTarget.y += 0.25;
+
+    // smooth camera - to do the lerping or linear interpolation
+    //0.1 means that hte value will get 1/10 closer to the destination
+    smoothedCameraPosition.lerp(cameraPosition, 5 * delta);
+    smoothedCameraTarget.lerp(cameraTarget, 5 * delta);
+
+    state.camera.position.copy(cameraPosition);
+    state.camera.lookAt(cameraTarget);
+  });
+
+  return (
+    <RigidBody
+      linearDamping={0.5}
+      angularDamping={0.5}
+      ref={body}
+      canSleep={false}
+      colliders="ball"
+      restitution={0.2}
+      friction={1}
+      position={[0, 1, 0]}
+    >
+      <mesh castShadow>
+        <icosahedronGeometry args={[0.3, 1]} />
+        <meshStandardMaterial flatShading color="mediumpurple" />
+      </mesh>
+    </RigidBody>
+  );
+}
