646e62b18fa4d9c114f58d37db33f4053c2c7e7c
Karen <karenbanci1@gmail.com>
2024-01-05 12:38:03 -0800

    create admin

--- mini-projects/ecommerce-store-first-project/core/urls.py
+++ mini-projects/ecommerce-store-first-project/core/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# isso serve para que o django consiga encontrar as imagens dentro do diretório
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+# configurando o django para encontrar a midia no diretório
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
