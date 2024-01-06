ed750c6ac0a186bedd23a15bb39eb39115f1f9bf
Karen <karenbanci1@gmail.com>
2024-01-05 18:24:04 -0800

    Add test cases for Category model and Views

---
+++ mini-projects/ecommerce-store-first-project/store/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from store.models import Product, Category
+
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name="django", slug="django")
+
+    def test_category_model_entry(self):
+        """
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+        self.assertEqual(str(data), "django")
---
+++ mini-projects/ecommerce-store-first-project/store/tests/test_views.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+from store.models import Product, Category
+
+class TestCategoriesViews(TestCase):
+    pass
