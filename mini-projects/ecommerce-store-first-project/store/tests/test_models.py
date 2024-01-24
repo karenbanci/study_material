ce1ecb91a78a5c69cbe4ed1f2bdd50acf8dc00ed
Karen <karenbanci1@gmail.com>
2024-01-24 14:54:42 -0800

    Add category and product model tests Add colorLUTShader.sca and related textures

--- mini-projects/ecommerce-store-first-project/store/tests/test_models.py
+++ mini-projects/ecommerce-store-first-project/store/tests/test_models.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 from store.models import Product, Category
+
 
 class TestCategoriesModel(TestCase):
     def setUp(self):
+        # creating an instance of the Category model
         self.data1 = Category.objects.create(name="django", slug="django")
 
     def test_category_model_entry(self):
@@ -13,3 +15,25 @@ class TestCategoriesModel(TestCase):
         data = self.data1
         self.assertTrue(isinstance(data, Category))
         self.assertEqual(str(data), "django")
+
+
+class TestProductsModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name="django", slug="django")
+        User.objects.create(username="admin")
+        self.data1 = Product.objects.create(
+            category_id=1,
+            title="django beginners",
+            created_by_id=1,
+            slug="django-beginners",
+            price="20.00",
+            image="django",
+        )
+
+    def test_products_model_entry(self):
+        """
+        Test Product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), "django beginners")
