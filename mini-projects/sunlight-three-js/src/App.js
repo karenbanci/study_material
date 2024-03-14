4033ad16ab422e72c860a1a42ea16e62fd546b3f
Karen <karenbanci1@gmail.com>
2024-03-14 12:41:26 -0700

    Update 3D model rendering and add orbit controls

--- mini-projects/sunlight-three-js/src/App.js
+++ mini-projects/sunlight-three-js/src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Suspense, useRef, useEffect } from "react";
+import { Canvas, useThree, extend } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { OrbitControls as ThreeOrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import styled from "styled-components";
+
+extend({ ThreeOrbitControls });
+
+function OrbitControls() {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree();
+
+  const controls = useRef();
+
+  useEffect(() => {
+    if (controls.current) {
+      controls.current.update();
+    }
+  });
+
+  return <threeOrbitControls ref={controls} args={[camera, domElement]} />;
+}
+
+function Model() {
+  const { scene } = useThree(); // Use the useThree hook to access the three.js scene
+
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("./draco/"); // Adjust path to Draco decoder
+    loader.setDRACOLoader(dracoLoader);
+
+    loader.load(
+      "/model3d.glb", // Adjust the path as necessary
+      (gltf) => {
+        scene.add(gltf.scene); // Directly add the loaded scene to the existing scene
+      },
+      undefined,
+      (error) => {
+        console.error("An error happened", error);
+      }
+    );
+
+    // Optionally, return a cleanup function if you want to remove the model when the component unmounts
+    return () => {
+      /* Perform any cleanup if necessary */
+    };
+  }, [scene]); // Empty dependency array means this effect runs once on mount
+
+  // No need to return anything since we're directly manipulating the scene
+  return null;
+}
 
 function App() {
+  const SceneConainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: black;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SceneConainer>
+      <Canvas>
+        <Suspense fallback={null}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Model />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
+    </SceneConainer>
   );
 }
 
