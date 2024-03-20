c0de7301c268aca4940fcd8fe40007c4a9c94c5d
Karen <karenbanci1@gmail.com>
2024-03-20 15:23:37 -0700

    Update Scene component and import OrbitControls

--- mini-projects/sunlight-three-js/src/Scene.js
+++ mini-projects/sunlight-three-js/src/Scene.js
@@ -1,38 +1,49 @@
+import Model from "./Model";
+import * as THREE from "three";
+import SkyAndSun from "./SkyAndSun";
 import React, { Suspense } from "react";
 import { Canvas } from "react-three-fiber";
-import * as THREE from "three";
-import Model from "./Model";
-import OrbitControls from "./OrbitControls";
-import SkyAndSun from "./SkyAndSun";
-import CalcSunPosition from "./CalcSunPosition";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+// import OrbitControls from "./OrbitControls";
+
+// import CalcSunPosition from "./CalcSunPosition";
+
+// console.log("Model", Model);
 
 function Scene() {
   // Example sun position calculation (customize as needed)
   // const sunPosition = new THREE.Vector3(100, 100, 100);
-  const sunPosition = CalcSunPosition();
+  // const sunPosition = CalcSunPosition();
 
   return (
-    <Canvas
-      camera={{ position: [0, 100, 2000], fov: 60 }}
-      onCreated={({ gl }) => {
-        gl.toneMapping = THREE.ACESFilmicToneMapping;
-        gl.toneMappingExposure = 0.5;
-      }}
-    >
-      <SkyAndSun
-        turbidity={10}
-        rayleigh={2}
-        mieCoefficient={0.005}
-        mieDirectionalG={0.7}
-        sunPosition={sunPosition}
-      />
-      <ambientLight intensity={0.1} />
-      <Suspense fallback={null}>
-        {/* <pointLight position={[10, 10, 10]} /> */}
-        <Model />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas
+        camera={{ position: [0, 100, 2000], fov: 60 }}
+        onCreated={({ gl }) => {
+          gl.toneMapping = THREE.ACESFilmicToneMapping;
+          gl.toneMappingExposure = 0.5;
+        }}
+      >
+        <SkyAndSun
+          turbidity={10}
+          rayleigh={2}
+          mieCoefficient={0.005}
+          mieDirectionalG={0.7}
+          // sunPosition={sunPosition}
+        />
+        <ambientLight intensity={0.1} />
+        <Suspense fallback={null}>
+          {/* <pointLight position={[10, 10, 10]} /> */}
+          <Model />
+        </Suspense>
+        <OrbitControls
+          minAzimuthAngle={-Math.PI / 4}
+          maxAzimuthAngle={Math.PI / 4}
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI - Math.PI / 6}
+        />
+      </Canvas>
+    </div>
   );
 }
 
