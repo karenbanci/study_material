2880cf137e077bf2330b4654310ec981aa39fd4d
Karen <karenbanci1@gmail.com>
2024-09-12 16:07:56 -0700

    lesson 7 module 4

---
+++ node-js-course/4_FUNDAMENTOS_NPM/7_remocao_pacote/index.js
@@ -0,0 +1,10 @@
+// const _ = require("lodash"); // simbole do lodash _
+import chalk from "chalk";
+import _ from "lodash";
+
+const a = [1, 2, 3, 4, 5];
+const b = [2, 4, 5, 6, 7];
+
+const diff = _.difference(a, b);
+
+console.log(chalk.bgRed.bold(diff));
