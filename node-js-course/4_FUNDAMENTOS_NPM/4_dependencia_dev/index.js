9e9c01dcb84554ae78d0fd5ff0c46e4a5d93b3c2
Karen <karenbanci1@gmail.com>
2024-09-12 15:42:21 -0700

    lesson 4 module 4

---
+++ node-js-course/4_FUNDAMENTOS_NPM/4_dependencia_dev/index.js
@@ -0,0 +1,9 @@
+const _ = require("lodash"); // simbole do lodash _
+const chalk = require("chalk");
+
+const a = [1, 2, 3, 4, 5];
+const b = [2, 4, 5, 6, 7];
+
+const diff = _.difference(a, b);
+
+console.log(chalk.red.bold(diff));
