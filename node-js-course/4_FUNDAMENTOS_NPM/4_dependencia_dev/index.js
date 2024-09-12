355f5643ce128b91f59f6bed1f09f2932fc259d0
Karen <karenbanci1@gmail.com>
2024-09-12 15:52:50 -0700

    finish lesson 5 module 4

--- node-js-course/4_FUNDAMENTOS_NPM/4_dependencia_dev/index.js
+++ node-js-course/4_FUNDAMENTOS_NPM/4_dependencia_dev/index.js
@@ -1,5 +1,5 @@
-const _ = require("lodash"); // simbole do lodash _
-const chalk = require("chalk");
+import chalk from "chalk";
+import _ from "lodash";
 
 const a = [1, 2, 3, 4, 5];
 const b = [2, 4, 5, 6, 7];
