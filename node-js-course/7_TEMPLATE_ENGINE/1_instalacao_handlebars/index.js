46042bfe7788d1f191e910d248bdd3b2db357d04
Karen <karenbanci1@gmail.com>
2024-09-19 15:40:26 -0700

    lesson 1 module 7

---
+++ node-js-course/7_TEMPLATE_ENGINE/1_instalacao_handlebars/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { create } = require("express-handlebars");
+
+const app = express();
+
+// Create an instance of the handlebars engine
+const hbs = create({ extname: ".handlebars" });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home", { layout: false });
+});
+
+app.listen(3000, () => {
+  console.log("funcionando");
+});
