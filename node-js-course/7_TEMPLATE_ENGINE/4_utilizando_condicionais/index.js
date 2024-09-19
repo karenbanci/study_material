7ad23849981af3b8340bb82ee24254a52e6d5b40
Karen <karenbanci1@gmail.com>
2024-09-19 16:09:15 -0700

    lesson 4 module 7

---
+++ node-js-course/7_TEMPLATE_ENGINE/4_utilizando_condicionais/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { create } = require("express-handlebars");
+
+const app = express();
+
+// Create an instance of the handlebars engine
+const hbs = create({ extname: ".handlebars" });
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Karen",
+    surname: "Honorio",
+  };
+
+  const palavra = "teste";
+
+  const auth = true;
+
+  res.render("home", { user: user, palavra, auth });
+});
+
+app.listen(3000, () => {
+  console.log("funcionando");
+});
