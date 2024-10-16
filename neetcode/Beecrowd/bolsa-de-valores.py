3dbdfa6c8c9c925aaf64182c51d6aa2c184091e8
Karen <karenbanci1@gmail.com>
2024-10-16 15:53:27 -0700

    finalizou o problema

--- neetcode/Beecrowd/bolsa-de-valores.py
+++ neetcode/Beecrowd/bolsa-de-valores.py
@@ -2,6 +2,10 @@ n, taxa = map(int, input().split())
 cotacoes = list(map(int, input().split()))
 variacoes = [cotacoes[i] - cotacoes[i - 1] for i in range(1, len(cotacoes))]
 
+if n == 1:
+    print(0)
+    exit()
+
 max_total = variacoes[0]
 max_terminando_agora = variacoes[0]
 
@@ -17,17 +21,12 @@ for i, atual in enumerate(variacoes[1:]):
     max_total = max(max_total, max_terminando_agora)
 
     if max_terminando_agora < max_total - taxa:
-        resultado += max_total - taxa
+        if max_total - taxa > 0:
+            resultado += max_total - taxa
         max_terminando_agora = 0
         max_total = 0
 
 if max_total > taxa:
     resultado += max_total - taxa
-
-# print("i---------------", i + 1)
-# print("max_terminando_agora:", max_terminando_agora)
-# print("max_total:", max_total)
-# print("resultado:", resultado, "\n")
-
 
 print(resultado)
